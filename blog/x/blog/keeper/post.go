d9d695669f8699915cc848e58b877fb2cfc3affe
Timothy G <timg51237@gmail.com>
2022-12-20 15:28:00 -0800

    initial blog app

---
+++ blog/x/blog/keeper/post.go
@@ -0,0 +1,48 @@
+package keeper
+
+import (
+	"encoding/binary"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"blog/x/blog/types"
+)
+
+func (k Keeper) AppendPost(ctx sdk.Context, post types.Post) uint64 {
+	count := k.GetPostCount(ctx)
+	post.Id = count
+	
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(types.PostKey))
+
+	byteKey := make([]byte, 8)
+	binary.BigEndian.PutUint64(byteKey, post.Id)
+	
+	appendedValue := k.cdc.MustMarshal(&post)
+	store.Set(byteKey, appendedValue)
+
+	k.SetPostCount(ctx, count + 1)
+	return count
+}
+
+func (k Keeper) GetPostCount(ctx sdk.Context) uint64 {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(types.PostCountKey))
+	byteKey := []byte(types.PostCountKey)
+	bz := store.Get(byteKey)
+
+	if bz == nil {
+		return 0
+	}
+
+	return binary.BigEndian.Uint64(bz)
+}
+
+func (k Keeper) SetPostCount(ctx sdk.Context, count uint64) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(types.PostCountKey))
+	byteKey := []byte(types.PostCountKey)
+
+	bz := make([]byte, 8)
+	binary.BigEndian.PutUint64(bz, count)
+
+	store.Set(byteKey, bz)
+}
